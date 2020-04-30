18073f9e1e14634a637809e052a02f20b9681777
swan <35875553+sushilc147@users.noreply.github.com>
2020-04-30 00:33:06 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,47 +1,22 @@
-# Terraform state will be stored in S3
-terraform {
-  backend "s3" {
-    bucket = "terraform-bucket-adk"
-    key    = "terraform.tfstate"
-    region = "us-east-2"
-  }
+variable "accessKey" {
+  type        = "string"
+  description = "AWS access key id"
+  default = "missing"
 }
 
-# Use AWS Terraform provider
+variable "secretKey" {
+  type        = "string"
+  description = "AWS access secret key"
+  default = "missing"
+}
+
 provider "aws" {
-  region = "us-east-2"
+  access_key = "${var.accessKey}"
+  secret_key = "${var.secretKey}"
+  region     = "us-east-2"
 }
 
-# Create EC2 instance
-resource "aws_instance" "default" {
-  ami                    = var.ami
-  count                  = var.instance_count
-  key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.default.id]
-  source_dest_check      = false
-  instance_type          = var.instance_type
-
-  tags = {
-    Name = "terraform-default"
-  }
-}
-
-# Create Security Group for EC2
-resource "aws_security_group" "default" {
-  name = "terraform-default-sg"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+resource "aws_instance" "example" {
+  ami           = "ami-0520e698dd500b1d1"
+  instance_type = "t2.micro"
 }
